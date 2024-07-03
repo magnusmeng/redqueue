cadb59862b52243f1dfec2f0b90cae85353f6f08
Magnus Meng Mortensen <magnusmeng@gmail.com>
2024-07-03 20:48:18 +0200

    feat: export errors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { defineQu, IQuOptions, IResolvedQuHandler } from "./qu";
 export { IQuMessage } from "./message";
 export { IQuConsumer, IConsumerOptions } from "./consumer";
+export * from "./errors";
